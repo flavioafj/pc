c19bc2ed3030a7141fd238946eebbb6bba16f053
Flávio Alves Ferreira <Flvio Alves Ferreira>
2017-02-15 21:51:53 -0200

    Upload inicial de arquivos

---
+++ index-modal.php
@@ -0,0 +1,83 @@
+<?php
+
+?>
+<div class="row">
+    <div class="col-md-2">
+            <div class="modal-p" >
+                <img data-toggle="modal" data-target="#pc_Modal" data-backdrop="true" alt="Visão geral do páteo do estacionamento" src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/1.jpeg">
+            </div> 
+    </div>
+    <div class="col-md-1">
+            <div class="modal-p">
+                <img data-toggle="modal" data-target="#pc_Modal" data-backdrop="true" alt="vagas descobertas do estacionamento" src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/2.jpg">
+            </div> 
+    </div>
+    <div class="col-md-1">
+            <div class="modal-p">
+                <img data-toggle="modal" data-target="#pc_Modal" data-backdrop="true" alt="Vagas cobertas do estacionamento Pátio Confins" src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/3.jpg">
+            </div> 
+    </div>
+    <div class="col-md-2">
+            <div class="modal-p">
+                <img data-toggle="modal" data-target="#pc_Modal" data-backdrop="true" alt="Vista noturna do Estacionamento Pátio Confins" src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/4.jpg">
+            </div> 
+    </div>
+    <div class="col-md-1">
+            <div class="modal-p">
+                <img data-toggle="modal" data-target="#pc_Modal" data-backdrop="true" alt="Vista da janela do estacionamento Pátio Confins" src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/5.jpg">
+            </div> 
+    </div>
+    <div class="col-md-2">
+            <div class="modal-p">
+                <img data-toggle="modal" data-target="#pc_Modal" data-backdrop="true" alt="escritório Pátio Confins" src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/6.jpg">
+            </div> 
+    </div>
+    
+    <div class="clearfix"></div>
+
+    
+</div>
+
+<div id="pc_Modal" class="modal fade" role="dialog">
+        <div id="pc_Carousel" class="carousel slide" data-ride="carousel" data-interval="false">
+
+            <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/1.jpeg" alt="Páteo do estacionamento Pátio Confins">
+            </div>
+
+            <div class="item">
+              <img src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/2.jpg" alt="Vagas descobertas">
+            </div>
+
+            <div class="item">
+              <img src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/3.jpg" alt="Vagas cobertas">
+            </div>
+
+            <div class="item">
+              <img src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/4.jpg" alt="Vista noturna Pátio Confins">
+            </div>
+
+            <div class="item">
+              <img src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/5.jpg" alt="Vista da janela do estacionamento">
+            </div>
+
+            <div class="item">
+              <img src="http://estacionamentopatioconfins.com.br/wp/wp-content/themes/appointment/images/modal/6.jpg" alt="escritório Pátio Confins">
+            </div>
+          </div>
+
+             <!-- Left and right controls -->
+          <a class="left carousel-control" href="#pc_Carousel" role="button" data-slide="prev">
+            <span class="fa fa-chevron-left setapc" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#pc_Carousel" role="button" data-slide="next">
+            <span class="fa fa-chevron-right setapc" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+
+
+
+        </div>
+</div>
